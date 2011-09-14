1f62613e3d398a93c5282b8f8d2a63bfba9094aa
iordodeo <will@iorodeo.com>
2011-09-13 21:10:35 -0700

    Finished hdf5 logger node for captive trajectory mode.

--- software/data_loggers/nodes/captive_logger.py
+++ software/data_loggers/nodes/captive_logger.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+Data logger for captivie trajectory mode. 
+"""
 import roslib 
 roslib.load_manifest('data_loggers')
 import rospy
@@ -32,23 +35,73 @@ class Captive_Logger(object):
         self.enabled = False
         self.logger = None
 
-        self.force_data = None
-        self.dist_data = None
-        self.setpt_data = None
-        self.actuator_data = None
-        self.motor_cmd_data = None
-        self.analog_in_data = None
+        # Initialize all data values to None
+        self.init_data_values()
 
         # Setup services
         self.set_log_file_srv = rospy.Service('set_logger_file', SetLogFile, self.handle_set_log_file)
         self.node_enable_srv = rospy.Service('logger_enable', NodeEnable, self.handle_node_enable) 
 
         # Subscribe to messages
+        self.force_sub = rospy.Subscriber('force', ForceMsg, self.force_msg_callback)
+        self.dist_sub = rospy.Subscriber('distance', DistMsg, self.dist_msg_callback)
+        self.setpt_sub = rospy.Subscriber('setpt', SetptMsg, self.setpt_msg_callback)
+        self.actuator_sub = rospy.Subscriber('actuator', ActuatorMsg, self.actuator_msg_callback)
+        self.motor_sub = rospy.Subscriber('motor_cmd', MotorCmdMsg, self.motor_msg_callback)
+        self.analog_sub = rospy.Subscriber('analog_input', AnalogInMsg, self.analog_msg_callback)
 
         rospy.on_shutdown(self.on_shutdown)
         rospy.init_node('data_logger')
         self.rate = rospy.Rate(self.logging_rate)
 
+    def force_msg_callback(self, data):
+        """
+        Callback for handling force sensor messages
+        """
+        with self.lock:
+            self.force_data = data
+
+    def dist_msg_callback(self, data):
+        """
+        Callback for handling distance messages
+        """
+        with self.lock:
+            self.dist_data = data
+
+    def setpt_msg_callback(self, data):
+        """
+        Callback for handling setpt messages
+        """
+        with self.lock:
+            self.setpt_data = data
+
+    def actuator_msg_callback(self, data):
+        """
+        Callback for handling actuator messages
+        """
+        with self.lock:
+            self.actuator_data = data
+
+    def motor_msg_callback(self, data):
+        """
+        Callback for handling motor cmd messages
+        """
+        with self.lock:
+            self.motor_data = data
+
+    def analog_msg_callback(self, data):
+        """
+        Callback for handling analog input messages. Note on first call
+        create analog input dataset in log file.
+        """
+        with self.lock:
+            if self.analog_data is None: 
+                # First call since reset - create analog input dataset
+                n = len(data.values)
+                self.logger.add_dataset('/data/analog_input', (n,))
+                self.logger.add_attribute('/data/analog_input', 'unit', 'V')
+            self.analog_data = data
+                
     def have_data(self):
         """
         Determine whether or not we have data form logging.
@@ -58,8 +111,8 @@ class Captive_Logger(object):
         test &= self.dist_data is not None
         test &= self.setpt_data is not None
         test &= self.actuator_data is not None
-        test &= self.motor_cmd_data is not None
-        test &= self.analog_in_data is not None
+        test &= self.motor_data is not None
+        test &= self.analog_data is not None
         return test
 
     def run(self):
@@ -102,54 +155,81 @@ class Captive_Logger(object):
         """
         Handle node enable service
         """
+        message = ''
         if req.enable:
-
-            # Enable logging node
-            filepath = os.path.join(self.filename,self.directory)
-            try:
-                self.logger = HDF5_Logger(filepath) 
-
-                # Create info group 
-                self.logger.add_group('/info')
-                self.logger.add_datetime('/info')
-                self.logger.add_attribute('/info', 'mode', 'captive_trajectory')
-
-                # Create force dataset
-                self.logger.add_dataset('/data/force'(1,))
-                self.logger.add_attribute('/data/force', 'unit', 'N')
-
-                # Create distance sensor dataset
-                self.logger.add_dataset('/data/distance/distance_raw', (1,))
-                self.logger.add_dataset('/data/distance/distance_kalman', (1,))
-                self.logger.add_dataset('/data/distance/velocity_kalman', (1,))
-                self.logger.add_attribute('/data/distance/distance_raw', 'unit', 'mm')
-                self.logger.add_attribute('/data/distance/distance_kalman', 'unit', 'mm')
-                self.looger.add_attribute('/data/distance/velocity_kalman', 'unit', 'mm/s')
-
-                # Create setpt dataset
-                self.logger.add_dataset('/data/setpt/position', (1,))
-                self.logger.add_dataset('/data/setpt/velocity', (1,))
-                self.logger.add_dataset('/data/setpt/error', (1,))
-                self.logger.add_attribute('/data/setpt/position', 'unit', 'mm')
-                self.logger.add_attribute('/data/setpt/velocity', 'unit', 'mm/s')
-                self.logger.add_attribute('/data/setpt/error', 'unit', 'mm')
-
-                self.enabled = True
-                status = True
-                message = ''
-            except IOError, e:
-                self.enabled = False
-                status = False
-                message = 'unable to open file: %s'%(e,)
+            # Enable logging mode
+            if not self.enabled:
+                try:
+                    self.create_hdf5_logger()
+                    self.enabled = True
+                except IOError, e:
+                     self.enabled = False
+                     message = 'unable to open file: %s'%(e,)
         else:
-
-            # Disable logging node
+            # Disable logging node and reset data to None
             self.enabled = False
             del self.logger
             self.logger = None
-            status = True
-            message = ''
-        return NodeEnableResponse(status,message)
+            self.init_data_values()
+
+        return NodeEnableResponse(self.enabled,message)
+
+    def create_hdf5_logger(self):
+        """
+        Creates hdf5 data logger, adds all groups, datasets and attributes 
+        except the analog input dataset which because of its possibly variable
+        size is created on the first callback of the analog input message.
+        """
+        # Create HDF5 data logger
+        filepath = os.path.join(self.filename,self.directory)
+        self.logger = HDF5_Logger(filepath) 
+
+        # Create info group 
+        self.logger.add_group('/info')
+        self.logger.add_datetime('/info')
+        self.logger.add_attribute('/info', 'mode', 'captive_trajectory')
+
+        # Create force dataset
+        self.logger.add_dataset('/data/force'(1,))
+        self.logger.add_attribute('/data/force', 'unit', 'N')
+
+        # Create distance sensor dataset
+        self.logger.add_dataset('/data/distance/distance_raw', (1,))
+        self.logger.add_dataset('/data/distance/distance_kalman', (1,))
+        self.logger.add_dataset('/data/distance/velocity_kalman', (1,))
+        self.logger.add_attribute('/data/distance/distance_raw', 'unit', 'mm')
+        self.logger.add_attribute('/data/distance/distance_kalman', 'unit', 'mm')
+        self.looger.add_attribute('/data/distance/velocity_kalman', 'unit', 'mm/s')
+
+        # Create setpt dataset
+        self.logger.add_dataset('/data/setpt/position', (1,))
+        self.logger.add_dataset('/data/setpt/velocity', (1,))
+        self.logger.add_dataset('/data/setpt/error', (1,))
+        self.logger.add_attribute('/data/setpt/position', 'unit', 'mm')
+        self.logger.add_attribute('/data/setpt/velocity', 'unit', 'mm/s')
+        self.logger.add_attribute('/data/setpt/error', 'unit', 'mm')
+
+        # Create actuator actuator dataset 
+        self.logger.add_dataset('/data/actuator',(1,))
+        self.logger.add_attribute('/data/actuator','unit','us')
+
+        # Create motor command dataset
+        self.logger.add_dataset('/data/motor_cmd', (1,))
+        self.logger.add_attribute('/data/motor_cmd', 'unit', '12-bit int')
+
+        # Note, Create analog input dataset not created until first
+        # analog input callback in order to get array size
+
+    def init_data_values_none(self):
+        """
+        Initializes all data values to None
+        """
+        self.force_data = None
+        self.dist_data = None
+        self.setpt_data = None
+        self.actuator_data = None
+        self.motor_data = None
+        self.analog_data = None
 
     def on_shutdown(self):
         with self.lock:
