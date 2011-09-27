b9734e65ab295e71f7d7b5c79703372285306d10
iordodeo <will@iorodeo.com>
2011-09-27 12:20:01 -0700

    Created hdf5 run file specification.  Created HDF5_Run_Reader class for reading hdf5 run file.

--- software/file_servers/nodes/create_run_file.py
+++ software/file_servers/nodes/create_run_file.py
@@ -1,6 +1,13 @@
+"""
+create_run_file.py 
+
+Creates an example run specification file for captive trajectory mode. Note, 
+it will probably be useful to create of python and matlab classes for generating 
+run specification files. 
+"""
+
 import h5py
 import numpy.random
-
 
 filename = 'test_run_file.hdf5'
 num_runs = 10
@@ -8,29 +15,47 @@ num_runs = 10
 f = h5py.File(filename,'w')
 
 info_grp = f.create_group('/info')
-info_grp.attrs['mode'] = 'captive'
+info_grp.attrs['mode'] = 'captive_trajectory'
 info_grp.attrs['notes'] = 'blah, blah, blah, ...'
 
 for i in range(0,num_runs):
 
     run_str = '/run_%d'%(i,)
     run_grp = f.create_group(run_str)
-    run_grp.attrs['mass'] = 2.0*i
-    run_grp.attrs['damping'] = 0.0
+    
+    mass_ds = run_grp.create_dataset('mass',(1,),'f',maxshape=(1,))
+    mass_ds[:] = 2.0*i
+    mass_ds.attrs['unit'] = 'kg'
 
-    if i < num_runs/2:
-        run_grp.attrs['type'] = 'ramp'
+    damping_ds = run_grp.create_dataset('damping',(1,),'f',maxshape=(1,))
+    damping_ds[:] = 0.0
+    damping_ds.attrs['unit'] = 'kg/s'
+
+    if i < num_runs/3:
+        run_grp.attrs['type'] = 'constant'
+
+        T_ds = run_grp.create_dataset('T',(1,),'f',maxshape=(1,))
+        T_ds[:] = 10.0
+        T_ds.attrs['unit'] = 's'
+
+        value_ds = run_grp.create_dataset('value',(1,),'f',maxshape=(1,))
+        value_ds[:] = 500.0
+        value_ds.attrs['unit'] = 'us'
+
+    elif i < 2*num_runs/3:
+        run_grp.attrs['type'] = 'trapeziodal'
+
         prm_grp = run_grp.create_group('%s/params'%(run_str,))
-        prm_grp['start'] = 0
-        prm_grp['end'] = 400
-        prm_grp['velo'] = 500.0
-        prm_grp['accel'] = 100.0
-        
+        prm_grp.attrs['T'] = 10.0
+        prm_grp.attrs['value'] = 300.0
+        prm_grp.attrs['rate'] = 100.0
     else:
         run_grp.attrs['type'] = 'array'
-        values = numpy.random.rand(500)
-        dataset = run_grp.create_dataset('values',array.shape,dtype=array.dtype,maxshape=array.shape) 
-        dataset[:] = array
-
-
-
+        array_values = numpy.random.rand(500)
+        array_values_ds = run_grp.create_dataset(
+                'values',
+                array_values.shape,
+                dtype=array_values.dtype,
+                maxshape=array_values.shape
+                ) 
+        array_values_ds[:] = array_values 
--- software/file_servers/nodes/hdf5_run_reader.py
+++ software/file_servers/nodes/hdf5_run_reader.py
@@ -1,4 +1,10 @@
-import os
+"""
+hdf5_run_reader.py
+
+Support class for reading hdf5 run files. 
+
+"""
+import os 
 import os.path
 import h5py
 
@@ -11,16 +17,19 @@ class HDF5_Run_Reader(object):
         self.run_list = [x for x in list(self.h5file) if x[:3] == 'run']
         self.number_of_runs = len(self.run_list)
 
+    def __iter__(self):
+        return self
+
     def get_info(self):
         return self.h5file['/info']
 
-    def get_next(self):
+    def next(self):
         if self.pos < self.number_of_runs:
             run_str = '/run_%d'%(self.pos,)
             self.pos += 1
             return self.h5file[run_str]
         else:
-            return None
+            raise StopIteration
 
     def get_run(self,n):
         if n >=0 and n < self.number_of_runs:
@@ -32,5 +41,58 @@ class HDF5_Run_Reader(object):
 
     def rewind(self):
         self.pos = 0
+
+    def move_to(self,pos):
+        if pos < 0 or pos > self.number_of_runs:
+            raise ValueError, 'run position out of range'
+        else:
+            self.pos = pos
+
+    def print_struct(self):
+        print 
+        print 'File: %s'%(self.filename,)
+        print '-'*60
+        print 'printing structure ...'
+        print
+        print_node_struct(self.h5file['/'])
+
+def print_node_struct(node):
+    """
+    Recursively prints the structure of a hdf5 file node.
+    """
+    child_list = list(node)
+    for child in child_list:
+        print node[child].name 
+        print_node_attrs(node[child])
+        if not isinstance(node[child],h5py.highlevel.Dataset):
+            print_node_struct(node[child])
+        else:
+            shape = node[child].shape
+            if shape[0] == 1:
+                print ' value:', float(node[child][0])
+            else:
+                print ' shape:', node[child].shape
+
+def print_node_attrs(node):
+    """
+    Prints the attributes of hdf5 file node.
+    """
+    for k,v in node.attrs.iteritems():
+        print ' %s: %s'%(k,v)
+
+# ---------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    import sys
+
+    filename = sys.argv[1]
+    run_reader = HDF5_Run_Reader(filename)
+
+    for run in run_reader:
+        print_node_struct(run)
+        print 
+    
+            
+        
 
 
